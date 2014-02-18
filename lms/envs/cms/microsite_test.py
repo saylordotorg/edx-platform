3bf97decea41e33032c84f0cacde62568d222560
Chris Dodge <cdodge@edx.org>
2014-02-18 11:12:51 -0500

    env cleanup

--- lms/envs/cms/microsite_test.py
+++ lms/envs/cms/microsite_test.py
@@ -27,26 +27,6 @@ MICROSITE_CONFIGURATION = {
         "course_index_overlay_text": "Explore free courses from leading universities.",
         "course_index_overlay_logo_file": "openedx/images/header-logo.png",
         "homepage_overlay_html": "<h1>Take an Open edX Course</h1>"
-    },
-    "ethicon": {
-        "domain_prefix":"ethicon",
-        "university":"ethicon",
-        "platform_name": "Ethicon on edX",
-        "logo_image_url": "ethicon/images/ce_rgb_logo-1.png",
-        "show_only_org_on_student_dashboard": True,
-        "email_from_address": "ethicon@ethicon.com",
-        "payment_support_email": "ethicon@ethicon.com",
-        "ENABLE_MKTG_SITE":  False,
-        "SITE_NAME": "ethicon.localhost",
-        "course_org_filter": "EthiconX",
-        "course_about_show_social_links": False,
-        "css_overrides_file": "ethicon/css/ethicon.css",
-        "show_partners": False,
-        "show_homepage_promo_video": True,
-        "homepage_promo_video_youtube_id": "RsRRMVzSXmE",
-        "course_index_overlay_text": "<img src='/static/ethicon/images/Ethicon_Better_Surgery_Vertical.png' width='400' height='103' />",
-        "homepage_overlay_html": "<img src='/static/ethicon/images/Ethicon_Better_Surgery_Vertical.png'  width='400' height='103' />",
-        "favicon_path": "ethicon/images/Ethicon_Spectrum_Red_Logo.ico"
     }
 }
 
